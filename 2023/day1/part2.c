d16870f874260a0ec0521597bebd93dab22592e7
Thomas van Vliet <git@thomasvanvliet.net>
2024-04-02 00:03:57 +0200

    Finished part 2

--- 2023/day1/part2.c
+++ 2023/day1/part2.c
@@ -1,102 +1,87 @@
-// Thomas van Vliet
-//
-// day 1 part 1
-// As they're making the final adjustments, they discover that their calibration document (your puzzle input) 
-// has been amended by a very young Elf who was apparently just excited to show off her art skills. 
-// Consequently, the Elves are having trouble reading the values on the document.
-
-// The newly-improved calibration document consists of lines of text; each line originally contained a 
-// specific calibration value that the Elves now need to recover. On each line, the calibration value 
-// can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
-
-
-#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
-char *FILENAME = "input.txt";
-char *NUMBERS[19] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const char *FILENAME = "input.txt";
+const unsigned int MAX_LINE_LENGTH = 256;
 
-int isNumer(char character){
-    return (character >= '0' && character <= '9');
+const char *NUMBERS[19] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int numbersIndexToString (int index) {
+    return index < 10 ? index : index - 10 + 1;
 }
 
 
-
-char numberStringToChar(int character){
-    char newChar;
-    if ( character >= 0 && character <= 9 ){
-        return '0' + character;
-    }
-    return '0';
-}
-
-
-
-char firstNumerInString(char *string){
-    int indexFirstNumber = 0;
-    int index = INT_MAX;
-    int temp = INT_MAX;
+int firstNumberInString(char *string, size_t size){
+    int indexFirstNumber = MAX_LINE_LENGTH;
+    int firstNumber;
+    int indexInArray;
+    char* pArrayIndex;
 
     for(int i = 0; i < 19; i++){
-        if( strstr(string, NUMBERS[i]) ){ 
-            temp = strstr(string, NUMBERS[i])-string;
-            if ( temp < index ){
-                indexFirstNumber = i;
-                index = temp;
-            }
+        pArrayIndex = strstr(string, NUMBERS[i]); 
+        
+        // if number not in string -> skip
+        if (pArrayIndex == NULL) {
+            continue;            
+        }
+
+        indexInArray = pArrayIndex - string;
+
+        // if index is smaller then current index
+        if (indexInArray < indexFirstNumber) {
+            indexFirstNumber = indexInArray;
+            firstNumber = i;
         }
     }
-    if( index == INT_MAX ) { return '0'; }
-    if ( index > 10 ){       return numberStringToChar(indexFirstNumber - 10); } 
-    return numberStringToChar(indexFirstNumber);
+
+    return numbersIndexToString(firstNumber);
 }
 
 
-char lastNumerInString(char *string, size_t size){
-    int indexFirstNumber = 0;
-    int index = INT_MAX;
-    int temp = 0;
+int lastNumberInString(char *string, size_t size){
+    int indexLastNumber = -1;
+    int lastNumber = 0;
+    int indexInArray;
+    char* pArrayIndex;
 
     for(int i = 0; i < 19; i++){
-        if( strstr(string, NUMBERS[i]) ){ 
-            temp = strstr(string, NUMBERS[i])-string;
-            if ( temp > index ){
-                indexFirstNumber = i;
-                index = temp;
+        pArrayIndex = strstr(string, NUMBERS[i]); 
+       
+        // while loop to keep looking after first match in case of exmp. onefourone
+        // needed because strstr loops from start to end 
+        while (pArrayIndex != NULL) {
+            indexInArray = pArrayIndex - string;
+            
+            if (indexInArray > indexLastNumber) {
+                indexLastNumber = indexInArray;
+                lastNumber = i;
             }
+            
+            pArrayIndex = strstr(pArrayIndex + strlen(NUMBERS[i]), NUMBERS[i]); 
         }
     }
-    if( index == INT_MAX ) { return '0'; }
-    if ( index > 10 ){       return numberStringToChar(indexFirstNumber - 10); } 
-    return numberStringToChar(indexFirstNumber);
-
-
+    return numbersIndexToString(lastNumber); 
 }
 
-int combineNumberCharacter(char *a, char *b){
-    int x = (*a) - '0';
-    int y = (*b) - '0';
-    return x * 10 + y *1;
+
+int numberOnLine (char* buffer, int bufLength) { 
+    int fNm = firstNumberInString(buffer, bufLength);
+    int lNm = lastNumberInString(buffer, bufLength);
+    return fNm * 10 + lNm;
 }
 
-int main() {
+
+int main () {
     int number = 0;
-    const unsigned MAX_LENGTH = 256;
-    char buffer[MAX_LENGTH];
-
+    char buffer[MAX_LINE_LENGTH];
     FILE *fp = fopen(FILENAME, "r");
-    while (fgets(buffer, MAX_LENGTH, fp)){
-        size_t lineLength = strlen(buffer);
-        char a = firstNumerInString(&buffer[0]);
-        char b = lastNumerInString(&buffer[0], lineLength);
-        printf("a: %c b: %c \n", a, b);
-        //int combindNumber = combineNumberCharacter(&a,&b);
-        number += combineNumberCharacter(&a, &b);
+    
+    while (fgets(buffer, MAX_LINE_LENGTH, fp)){
+         number += numberOnLine(buffer, strlen(buffer)); 
     }
+    
     fclose(fp);
 
     printf("The calibration value = %i", number);
-
-    return 0;
+    // answer 54431
 }
