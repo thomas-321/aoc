2a88363bcb1bbac577c006d3248a17365f14057d
Thomas van Vliet <git@thomasvanvliet.net>
2024-07-25 23:01:23 +0200

    Almost, of by 2

--- 2023/day1/day1rust/src/main.rs
+++ 2023/day1/day1rust/src/main.rs
@@ -1,16 +1,76 @@
 use std::fs::read_to_string;
 
+
+
 fn main() {
-   read_lines("./input.txt");
-}
+    let filename = "src/input.txt";
+    let num_vector = ["0", "1", "2", "3", "4", "5" ,"6", "7", "8", "9",
+                      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].to_vec();
 
-fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
+    println!("value {}", find_first_number_in_string("itestone", &num_vector));
 
+    let mut index = 0;
     for line in read_to_string(filename).unwrap().lines() {
-        // result.push(line.to_string())
-        println!("{line}");
+        index += find_first_number_in_string(line, &num_vector) * 10;
+        index += find_last_number_in_string(line, &num_vector);
     }
 
-    result
+    println!("The answer is: {index}");
 }
+
+
+fn find_first_number_in_string(source_string: &str, num_vector: &Vec<&str>) -> i32 {
+    let mut index = usize::MAX;
+    let mut vec_index = 0;
+
+    for (pos, num) in num_vector.iter().enumerate() {
+        let optional_location = source_string.find(num);
+        
+        if !optional_location.is_some(){
+            continue;
+        }
+
+        let index_in_string = optional_location.unwrap();
+        if index_in_string < index {
+            index = index_in_string;
+            vec_index = pos;
+        }
+    }
+
+    if vec_index > 10 {
+        vec_index -= 10;
+    } 
+
+    println!("string: {} \t index: {}", source_string, vec_index);
+    return vec_index as i32;
+
+}
+
+
+fn find_last_number_in_string(source_string: &str, num_vector: &Vec<&str>) -> i32 {
+    let mut index = usize::MIN;
+    let mut vec_index = 0;
+
+    for (pos, num) in num_vector.iter().enumerate() {
+        let optional_location = source_string.find(num);
+        
+        if !optional_location.is_some(){
+            continue;
+        }
+
+        let index_in_string = optional_location.unwrap();
+        if index_in_string >= index {
+            index = index_in_string;
+            vec_index = pos;
+        }
+    }
+
+    if vec_index > 10 {
+        vec_index -= 10;
+    } 
+
+    println!("string: {} \t index: {}", source_string, vec_index);
+    return vec_index as i32;
+
+}
+
