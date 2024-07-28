7cdf89c412a6f34d9c7d8f57929ccb4d26f3e4e2
Thomas van Vliet <git@thomasvanvliet.net>
2024-07-28 22:33:35 +0200

    Finished day 1 part 2 in rust!
    Why was there an offset of 2?
    
    Take the string "7zmlcpsjneight7pbtqbkgl".
    
    When searching for the last character, first the normal numbers where searched.
    7 was then found at index 0, later 8 spelled out was searched and
    because the index was later in the string the 7 was replaces by the
    index of eight.
    
    So because, after finding 7, there is no search for another 7 the last one
    could not be found.
    
    To fix this problem i changed from the &str.find function to &str.rfind
    which instead of searching for the first match, searches for the last
    match.
    
    Another solution would have been to keep searching after a match. This
    would however have resulted in more instructions for the program.

--- 2023/day1/day1rust/src/main.rs
+++ 2023/day1/day1rust/src/main.rs
@@ -7,7 +7,6 @@ fn main() {
     let num_vector = ["0", "1", "2", "3", "4", "5" ,"6", "7", "8", "9",
                       "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].to_vec();
 
-    println!("value {}", find_first_number_in_string("itestone", &num_vector));
 
     let mut index = 0;
     for line in read_to_string(filename).unwrap().lines() {
@@ -41,9 +40,7 @@ fn find_first_number_in_string(source_string: &str, num_vector: &Vec<&str>) -> i
         vec_index -= 10;
     } 
 
-    println!("string: {} \t index: {}", source_string, vec_index);
     return vec_index as i32;
-
 }
 
 
@@ -52,7 +49,7 @@ fn find_last_number_in_string(source_string: &str, num_vector: &Vec<&str>) -> i3
     let mut vec_index = 0;
 
     for (pos, num) in num_vector.iter().enumerate() {
-        let optional_location = source_string.find(num);
+        let optional_location = source_string.rfind(num);
         
         if !optional_location.is_some(){
             continue;
